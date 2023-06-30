f1978a7d4ef43730366aea7ee4305bde5d48496c
Herman <manger32@gmail.com>
2023-06-30 22:01:29 +0300

    All Geekbrains Materials

---
+++ Geekbrains_Seminar_6/Program.cs
@@ -0,0 +1,61 @@
+﻿/**Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.**/
+
+uint a=5,b=30,c=45;
+bool CheckIfTriangleExists(uint a, uint b, uint c)
+{
+    if (a<0 || b<0||c<0)
+    {
+        Console.WriteLine("Are you mad?");
+    }
+    else if (a+b > c && a+c > b && b+c > a)
+    {
+        Console.WriteLine("Such a triangle exists");
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Such a triangle does not exist");
+        return false;
+    }
+}
+
+/**Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10**/
+//VAR_1
+/**int[] arr = new int[32];
+void bintype(int a)
+{
+    for (int i = 0; i < 32; i++)
+    {
+        arr[i] = (int)(a>>i &0x01);
+    }
+}
+//VAR_2
+bintype(91);
+for (int i = 0; i < 32; i++)
+{
+    Console.Write(arr[i]);
+}
+
+Console.WriteLine("введите число");
+int a=Convert.ToInt32(Console.ReadLine());
+int b=a;
+int count=0;
+    while(b!=0){
+        count++;
+        b=b/2;
+    }
+int[] c=new int[count];
+    while(a!=0){
+        for(int i=0;i<c.Length;i++){
+            c[i]=a%2;
+            a=a/2;
+        }
+    }
+string s="";
+for (int i = c.Length-1; i >= 0; --i){
+s+=c[i].ToString();}
+Console.WriteLine(s);**/
---
+++ leetcode/Program.cs
@@ -0,0 +1,79 @@
+﻿int fromBase = 10;
+int toBase = 2;
+int num1 = 3;
+string result = Convert.ToString(Convert.ToInt32(num1, fromBase), toBase);
+
+/* using System.Numerics;
+
+int[] GetRandomArray(int sa)
+{
+    int[] array = new int[sa];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(int.MinValue, int.MaxValue);
+    }
+    return array;
+}
+
+int[] nums = new int[10];
+nums = GetRandomArray(10);
+Console.WriteLine(Solution.CountBeautifulPairs(nums));
+public class Solution
+{
+    public int CountBeautifulPairs(int[] nums)
+    {
+        int[] array = nums;
+        int result = 0;
+        for (int i = 0; i < array.Length; ++i)
+            for (int j = i+1; j < array.Length; ++j)
+            {
+                if (BigInteger.GreatestCommonDivisor(array[i], array[j]) == 1)
+                    result++;
+            }
+        return result;
+    }
+} */
+
+
+/* public class Solution
+{
+    public int CountBeautifulPairs(int[] nums)
+    {
+        int N = nums.Length;
+
+        var A = nums;
+        var B = nums;
+
+        var X = new bool[N];
+        var Y = new bool[N];
+
+        var count = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (!Y[j])
+                    if (int.Parse(A[i]) == int.Parse(B[j]))
+                    {
+                        X[i] = Y[j] = true;
+                        count++;
+                        break;
+                    }
+            }
+        }
+
+        foreach (var x in X)
+        {
+            if (!x)
+                foreach (var y in Y)
+                {
+                    if (!y)
+                    { count++; Console.WriteLine(count); return; }
+                }
+        }
+
+        count--;
+        return count;
+    }
+} */
