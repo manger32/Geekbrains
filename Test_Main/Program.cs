45ec6ea1561c13b9f8f4bd1e5581908ce1fef5c0
Herman <manger32@gmail.com>
2023-07-29 11:28:35 +0300

    working code, still string is empty scenario not handled.

--- Test_Main/Program.cs
+++ Test_Main/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N;
+
+List<String> FindAllOfLengthLessThan3Strings(int N)
+{
+    String[] arrayStr = new String[N];
+    List<String> resultingList = new List<String>();
+    int EmptyStringAmount = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        String currentString = Console.ReadLine();
+        if (String.IsNullOrEmpty(currentString))
+        {
+            resultingList.Add("");
+            EmptyStringAmount++;
+        }
+        else 
+            arrayStr[i-EmptyStringAmount] = currentString;
+    }
+    for (int i = 0; i < arrayStr.Length; ++i)
+    {
+        if (arrayStr[i].Length < 4)
+            resultingList.Add(arrayStr[i]);
+    }
+    return resultingList;
+}
+
+
+Console.WriteLine("Input string amount");
+N = Convert.ToInt32(Console.ReadLine());
+List<String> resList = FindAllOfLengthLessThan3Strings(N);
+Console.WriteLine(resList.Aggregate((a, b) => String.Format("{0}, {1}", a, b)));
