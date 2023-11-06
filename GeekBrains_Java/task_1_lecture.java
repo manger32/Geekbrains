4624ed8a37b4c7bf352c27f2da63ace7f8adf527
manger32 <manger32@gmail.com>
2023-11-06 16:45:21 +0300

    geekbrains_java/task_1_lecture.java added polynomial functions

--- GeekBrains_Java/task_1_lecture.java
+++ GeekBrains_Java/task_1_lecture.java
@@ -7,51 +7,80 @@ public class task_1_lecture {
         1. Задана натуральная степень k. Сформировать случайным
         образом список коэффициентов (значения от 0 до 100)
         многочлена многочлен степени k.
-        *Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+        *Пример: k=2 => 2*x^2 + 4*x + 5 или x^2 + 5 или 10*x^2
         2. Даны два файла, в каждом из которых находится запись
         многочлена. Сформировать файл содержащий сумму
         многочленов.
     */
     public static void main(String[] args) throws Exception{
-        int k = Integer.parseInt(args[0]);
-        int[] array_coefficients = new int[k];
-        for (int i = 0; i < array_coefficients.length; ++i)
-        {
-            Random rnd = new Random();
-            int number = rnd.nextInt(100);
-            array_coefficients[i] = number;
-        }
+        GetArrayCoefficients(Integer.parseInt(args[0]));
         BufferedReader br1 = new BufferedReader(new FileReader("polynomial1.txt"));
         BufferedReader br2 = new BufferedReader(new FileReader("polynomial2.txt"));
         String str1 = br1.readLine();
         String str2 = br2.readLine();
         int[] array_coefficients_1 = ConvertToArray(str1);
         int[] array_coefficients_2 = ConvertToArray(str2);
-        System.out.println(str1 + '\n' + str2);
+        int[] resulting_array_coefficients = new int[Integer.max(array_coefficients_1.length,array_coefficients_2.length)];
+        int j = 0;
+        for (j = 0; j < resulting_array_coefficients.length; ++j)
+            resulting_array_coefficients[j] = 0;
+        for (j = 0; j < Integer.min(array_coefficients_1.length,array_coefficients_2.length); ++j)
+        {
+            resulting_array_coefficients[j] = array_coefficients_1[j] + array_coefficients_2[j];
+        }
+        if (array_coefficients_1.length > array_coefficients_2.length)
+            for (;j < Integer.max(array_coefficients_1.length,array_coefficients_2.length); ++j)
+                resulting_array_coefficients[j] = array_coefficients_1[j];
+        else
+            for (;j < Integer.max(array_coefficients_1.length,array_coefficients_2.length); ++j)
+                resulting_array_coefficients[j] = array_coefficients_2[j];
+        for (int i = 0; i < resulting_array_coefficients.length;++i)
+            System.out.print(resulting_array_coefficients[i] + " ");
         br1.close();
         br2.close();
     }
+
+    /*Function that converts String polynomial to coefficients array */
     static int[] ConvertToArray(String s)
     {
         String[] members = s.split("\\+");
         int len = members.length;
-        int[] coefficients = new int[len];
+        int[] coefficients = new int[Integer.parseInt(members[0].substring(members[0].indexOf('x') + 2, members[0].length()))];
         for (int i = 0; i < len; ++i)
             coefficients[i] = 0;
         for (int i = 0; i < len; ++i)
         {
             String[] qres = members[i].split("\\*");
             if (qres[0].charAt(0) == 'x')
-            {
-                System.out.println(1);
                 coefficients[(int) qres[0].charAt(2) - '0'] = 1;
-            }
+            else if (qres.length == 1)
+                coefficients[0] = Integer.parseInt(qres[0]);
+            else
+                coefficients[(int) qres[1].charAt(2) - '0' - 1] = Integer.parseInt(qres[0]);
+        }
+        return coefficients;
+    }
+
+    /*Function that generates a random array of coefficients and outputs them to sysout*/
+    static void GetArrayCoefficients(int k)
+    {
+        int[] array_coefficients = new int[k];
+        for (int i = 0; i < array_coefficients.length-1; ++i)
+        {
+            Random rnd = new Random();
+            int number = rnd.nextInt(10);
+            array_coefficients[i] = number;
+            int which = array_coefficients.length-1-i;
+            if (i != array_coefficients.length-2)
+                    System.out.print(array_coefficients[i] + "*x^" + which + " + ");
             else
             {
-                System.out.println(qres[1]);
-                coefficients[(int) qres[1].charAt(2) - '0'] = Integer.parseInt(qres[0]);
+                rnd = new Random();
+                number = rnd.nextInt(10);
+                array_coefficients[array_coefficients.length - 1] = number;
+                System.out.print(array_coefficients[i] + "*x + " + array_coefficients[array_coefficients.length - 1]);
             }
         }
-        return new int[1];
-    }
+        System.out.println();
+    }   
 }
